6e523abbe06de55ca91e1484f87c4eeab52e7d27
weibik <j-szkola@wp.pl>
2022-03-22 13:27:07 +0100

    Function morse_encoder is done.

--- morse_encoder.m
+++ morse_encoder.m
@@ -1,14 +1,63 @@
-function [] = morse_encoder(message)
-    % MORSE_ENCODER function is 
+function [pulse_seq] = morse_encoder(message)
+    % MORSE_ENCODER function is used to transform a normal message into a
+    %               morse code.
+    %       
+    %               We have two options to do that:
+    %
+    %                   - First is without output argument. In this case our
+    %                   message is transformed into a string of dots and
+    %                   dashes. Beetween every letter there is a space and
+    %                   beetween every word there is a three space break    
+    %
+    %                   - Second option is to put an output argument. In this
+    %                   case our output is the string of zero's and one's.
+    %                   dot is a 1, dash is 111
+    %                   Beetween every sign there is a 0 and beetwen every word
+    %                   there is a 0000000.
+                
 
     load("morse.mat","Alpha","Morse");
-    message = char(message);
+    message = upper(char(message));
     result = "";
 
-    for n = 1:strlength(message)
-        i = find(Alpha == message(n));
-        result = append(result, Morse{i});
-    end
-    result
-    
+    switch nargout
+        case 0
+            for n = 1:strlength(message)
+                switch message(n)
+                    case " "
+                        result = append(result, "   ");
+                    otherwise
+                        i = Alpha == message(n);
+                        result = append(result, Morse{i} + " ");
+                end                
+            end
+            display(result)
+        case 1
+            pulse_seq = "";
+            for n = 1:strlength(message)
+                switch message(n)
+                    case " "
+                        pulse_seq = append(pulse_seq, "0000");
+                    otherwise
+                        i = Alpha == message(n);
+                        ms = Morse{i};
+
+                        for x = 1:strlength(ms)
+                            if ms(x) == '.'
+                                result = append(result, "1");
+                            elseif ms(x) == '-'
+                                result = append(result, "111");
+                            end
+                            pulse_seq = append(pulse_seq, result);
+                            if x ~= strlength(ms)
+                                pulse_seq = append(pulse_seq, "0");
+                            end
+                            result = "";
+                        end
+                        if n ~= strlength(message)
+                            	pulse_seq = append(pulse_seq, "000");
+                        end
+                end
+            end
+     end
 end
